5bda2cbdce2813947785c1566e6566a909a2df64
gokemon <michael.of.gokey@gmail.com>
2017-06-25 20:20:51 -0400

    added the database, connected, added routes, but now data passeda round

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # notable-api
 MEN-based API for saving notes
+
+npm run dev
+
+
+Well that was disappointing. Left me flat.
+All that, and I can't conenct to my live database.
+See my notes
--- app/config/db.js
+++ app/config/db.js
@@ -1,3 +1,9 @@
+// To connect using the mongo shell:
+// mongo ds119618.mlab.com:19618/gokesters-notable -u <dbuser> -p <dbpassword>
+// To connect using a driver via the standard MongoDB URI 
+// mongodb://<dbuser>:<dbpassword>@ds119618.mlab.com:19618/gokesters-notable
+
+
 module.exports = {
-    url: YOUR URL HERE
+    url: 'mongodb://<dbuser>:<dbpassword>@ds119618.mlab.com:19618/gokesters-notable'
 };
